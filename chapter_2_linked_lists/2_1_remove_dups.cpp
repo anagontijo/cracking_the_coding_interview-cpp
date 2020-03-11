0f9d2cff9dd7de90f8c6693c85df468e89e62db9
Ana Gontijo <54961700+anagontijo@users.noreply.github.com>
2020-03-11 00:17:11 -0300

    Update 2_1_remove_dups.cpp

--- chapter_2_linked_lists/2_1_remove_dups.cpp
+++ chapter_2_linked_lists/2_1_remove_dups.cpp
@@ -2,6 +2,14 @@
 #include <vector>
 #include <map>
 
+/*
+Remove Dups: Write code to remove duplicates from an unsorted linked list.
+(How would you solve this problem if a temporary buffer is not allowed)
+
+So, in this example as most of the times I use a map to count frequency. Count begins at 0, everytime a value is visited it adds up 1
+in the counter. If the counter is greater than 1 for said value, the node is removed.
+*/
+
 using namespace std;
 
 int main(){
@@ -12,8 +20,9 @@ int main(){
     n[i] = i%4;
     ll.add_node(&n[i]);
   }
+  
+  //General code begins here
   node* now = ll.base->next;
-  ll.print_list();
   while(now != ll.base){
     count[now->value] += 1;
     now = now->next;
@@ -21,5 +30,4 @@ int main(){
       ll.remove_node(now->prev);
     }
   }
-  ll.print_list();
 }
